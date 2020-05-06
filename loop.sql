319b449e6c63f6d5549aed39e9b96dabf5a5578c
Aleksandra1101 <44844924+Aleksandra1101@users.noreply.github.com>
2020-05-06 11:34:32 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -6,21 +6,21 @@ BEGIN
 
     FOR i IN 1..test_values LOOP
 
-        INSERT INTO film(FILMTV_ID,TITLE,YEAR,GENRE,duration,country, director, actors, avg_vote,VOTES)
+        INSERT INTO movie(FILMTV_ID,TITLE, YEAR, GENRE, country, director)
 
-            VALUES (3860, 'The Gods Must Be Crazy II', 	1990, 'Comedy', 100, 'United States', 	'Jamie Uys',	'Nixau, Lena Farugia, Hans Strydom, Erick Bowen', 6.8, 19); 
-
-
-
-        INSERT INTO film(FILMTV_ID,TITLE,YEAR,GENRE,duration,country, director, actors, avg_vote,VOTES)
-
-            VALUES (3863, 'Kiss Daddy Goodnight',	1987,	'Thriller',	88,	'United States',	'Peter Ily Huemer', 	'Uma Thurman, Paul Richards, Steve Buscemi, Paul Dillon',	6.0,	1	);
+            VALUES (3860, 'The Gods Must Be Crazy II', 	1990, 'Comedy', 'United States', 'Jamie Uys'); 
 
 
 
-        INSERT INTO film(FILMTV_ID,TITLE,YEAR,GENRE,duration,country, director, actors, avg_vote,VOTES)
+         INSERT INTO movie(FILMTV_ID,TITLE, YEAR, GENRE, country, director)
 
-            VALUES (3832,	'Who is Julia?',	1986,	'Drama',	120,	'United States', 	'Walter Grauman', 'Mare Winningham, Jameson Parker',	4.0,	1	); 
+            VALUES (3863, 'Kiss Daddy Goodnight',	1987,	'Thriller',	'United States', 'Peter Ily Huemer');
+
+
+
+         INSERT INTO movie(FILMTV_ID,TITLE, YEAR, GENRE, country, director)
+
+            VALUES (3832,	'Who is Julia?',	1986,	'Drama',	'United States', 	'Walter Grauman'); 
 
     END LOOP;
 

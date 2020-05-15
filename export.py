c53b8b6446af22e3b3f3e6152ff29656bd11b235
Aleksandra1101 <44844924+Aleksandra1101@users.noreply.github.com>
2020-05-15 14:30:16 +0300

    Update export.py

--- export.py
+++ export.py
@@ -9,7 +9,7 @@ cursor = connection.cursor()
 
 table = ['movie', 'mov_country']
 for i in table:
-    with open(i + '.csv', 'w', newline='') as csvf:
+    with open(i + '.csv', 'w', newline='', encoding='utf-8') as csvf:
 
         cursor.execute('select * from ' + i)
         
